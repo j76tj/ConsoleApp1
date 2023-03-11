9404c8e465ae6c313a7092f300cb1d7be9dac913
Synergy <Synergy@DESKTOP-KNO7D8A>
2023-03-11 15:00:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Таблица умножения:");
+        for (int i = 1; i <= 15; i++)
+        {
+            Console.Write(i.ToString("X") + " | ");
+            for (int j = 1; j <= 15; j++)
+            {
+                Console.Write((i * j).ToString("X") + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("\nТаблица сложения:");
+        for (int i = 0; i <= 15; i++)
+        {
+            Console.Write(i.ToString("X") + " | ");
+            for (int j = 0; j <= 15; j++)
+            {
+                Console.Write((i + j).ToString("X") + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+}
