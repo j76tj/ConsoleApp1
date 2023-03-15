21779abd423ded4b2ee26f9f2473b65a76ae36ad
Shukes4321 <127967303+Shukes4321@users.noreply.github.com>
2023-03-15 18:12:37 +0300

    norm
    norm,is

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,24 +2,24 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Таблица умножения:");  
+        Console.WriteLine("Таблица умножения:");nf,  // таблица умножения 
         for (int i = 1; i <= 15; i++)
         {
-            Console.Write(i.ToString("X") + " | "); 
+            Console.Write(i.ToString("X") + " | ");  //
             for (int j = 1; j <= 15; j++)
             {
-                Console.Write((i * j).ToString("X") + " "); //таблица умножения
+                Console.Write((i * j).ToString("X") + " ");  //
             }
             Console.WriteLine();
         }
 
-        Console.WriteLine("\nТаблица сложения:"); //таблица сложения
+        Console.WriteLine("\nТаблица сложения:");  // таблица сложения 
         for (int i = 0; i <= 15; i++)
         {
             Console.Write(i.ToString("X") + " | ");
             for (int j = 0; j <= 15; j++)
             {
-                Console.Write((i + j).ToString("X") + " ");
+                Console.Write((i + j).ToString("X") + " ");  //
             }
             Console.WriteLine();
         }
