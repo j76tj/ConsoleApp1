d96372755f466aaac301c1e264ffaabaefbec4c3
vard12312 <127601336+vard12312@users.noreply.github.com>
2023-03-15 17:59:15 +0300

    гыв5
    гы5ц

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,13 +2,13 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Таблица умножения:");
+        Console.WriteLine("Таблица умножения:");  делай по новрй, раб божий
         for (int i = 1; i <= 15; i++)
         {
             Console.Write(i.ToString("X") + " | ");
             for (int j = 1; j <= 15; j++)
             {
-                Console.Write((i * j).ToString("X") + " ");
+                Console.Write((i * j).ToString("X") + " "); ничего
             }
             Console.WriteLine();
         }
